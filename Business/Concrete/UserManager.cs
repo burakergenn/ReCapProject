e012f6c17d2ce6566160dcad95b9f846d4e0474f
Burak Ergen <b.ergen@outlook.com.tr>
2021-02-13 01:47:14 +0300

    13/2/21 Yeni Entityler eklendi ve CRUD operasyonları yazıldı.

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,68 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userdal)
+        {
+            _userDal = userdal;
+        }
+
+        public IResult Add(User user)
+        {
+            if (user.FirstName.Length<2)
+            {
+                return new ErrorResult();
+            }
+            _userDal.Add(user);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(User user)
+        {
+            _userDal.Delete(user);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            return new SuccessDataResult<List<User>>(_userDal.GetAll());
+        }
+
+        public IDataResult<User> GetById(int userId)
+        {
+            return new SuccessDataResult<User>(_userDal.Get(p => p.Id == userId));
+        }
+
+        public IDataResult<User> GetUsersByEmail(string email)
+        {
+            return new SuccessDataResult<User>(_userDal.Get(p => p.Email == email));
+        }
+
+        public IDataResult<List<User>> GetUsersByLastName(string lastName)
+        {
+            return new SuccessDataResult<List<User>>(_userDal.GetAll(p => p.LastName == lastName));
+        }
+
+        public IDataResult<List<User>> GetUsersByFirstName(string firstName)
+        {
+            return new SuccessDataResult<List<User>>(_userDal.GetAll(p => p.FirstName == firstName));
+        }
+
+        public IResult Update(User user)
+        {
+            _userDal.Update(user);
+            return new SuccessResult();
+        }
+    }
+}
